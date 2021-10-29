24b55d9dbbaece1bd95d043d6837efb8bc949ceb
Jayachandran V <jayachandran.venugopalan@bpmlinks.com>
2021-10-29 12:22:09 +0530

    Initial 2

--- app/src/main/java/com/smf/mylibrary/MainActivity.kt
+++ app/src/main/java/com/smf/mylibrary/MainActivity.kt
@@ -3,12 +3,13 @@ package com.smf.mylibrary
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smf.toasterlibrary.MainMessage
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Main.s(this,"in the lib is called")
+       MainMessage.s(this,"in the lib is called")
     }
 }
